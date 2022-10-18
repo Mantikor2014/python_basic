bac5e240e94f90649869a73a9f5b8e40e9e63be5
mantikor2014 <mihailstukalo16@gmail.com>
2022-10-18 18:51:19 +0200

    add homework 2 for lesson 2

---
+++ lesson_02/homework_02/variables.py
@@ -0,0 +1,18 @@
+# a = 12
+# b = 21
+# c = a 
+# a = b
+# b = c 
+# print(a, b) 
+
+# a = 45
+# b = 35
+# a, b = b, a
+# print(a, b)
+
+a = 40 
+b = 50 
+a = a + b
+b = a - b
+a = a - b
+print(a , b)
--- main.py
+++
@@ -1,3 +0,0 @@
-print(" 1. \n 2. \n 3. \n 4. \n 5. \n 6. \n 7. \n 8. \n 9.")
-for i in range(1, 11):
-    print(i)
